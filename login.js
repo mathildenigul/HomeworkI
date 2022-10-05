99bec6640dfe0630f618b3976fe645dd323e00a7
Úku Parts <uku.parts@gmail.com>
2022-10-05 13:53:36 +0300

    rework how the login form is validated

--- login.js
+++
@@ -1,9 +0,0 @@
-function validate() {
-    var form = document.getElementById("login_form");
-    if(form.email.value == "" || form.password.value == "") {
-        form.action = ""
-    }
-    else {
-        form.action = "index.html"
-    }
-}
